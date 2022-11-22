66feb2c9d30c0f019debba22346cc711e402ec55
cinusko <andrej.bakos0@gmail.com>
2022-11-22 08:18:35 +0100

    Open-Lab-10.00

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -10,7 +10,7 @@ namespace Open_Lab_10._00
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("xxxxx")
+            Console.WriteLine("xxxxxxx")
         }
     }
 }
