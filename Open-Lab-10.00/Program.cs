498df7bfeb3174fa8f1cef831f86237823b7194d
cinusko <andrej.bakos0@gmail.com>
2022-12-06 23:49:58 +0100

    Open-Lab-10.03

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -15,7 +15,7 @@ namespace Open_Lab_10._00
                 LOTR.Title = "Lord of the rings";
                 LOTR.Pages = 450;
                 LOTR.Category = "Fantasy";
-                LOTR.Author = "J. R. R Tolkien";
+                LOTR.Author = "J. R. R. Tolkien";
                 LOTR.ReleaseDate = 1954;
                 Console.WriteLine(LOTR.ToString());
 
